e400fa21bd4739a6a6f1bda0b1ae285f659987a1
Eban <account+gitil@eban.eu.org>
2022-07-31 16:48:17 +0200

    [🎨 🐛 Style fix] Add eslint style config and use it

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,190 +1,191 @@
-const plusBtn = document.querySelector("#plus")
-const backgroundUpload = document.querySelector("#backgroundUpload")
-const saveUpload = document.querySelector("#saveUpload")
-const pagesList = document.querySelector(".pagesList")
-const exportBtn = document.querySelector("#export")
-
-backgroundUpload.addEventListener("change", updateBg)
-saveUpload.addEventListener("change", restoreSave)
-
-background = ""
-
-function updateBg() {
-    uploadedImg = backgroundUpload.files[0]
-    reader = new FileReader()
-    reader.onloadend = function() {
-        background = reader.result
-        updateBgInDom()
-    }
-    reader.readAsDataURL(uploadedImg);
-}
+const backgroundUpload = document.querySelector('#backgroundUpload');
+const saveUpload = document.querySelector('#saveUpload');
+const exportBtn = document.querySelector('#export');
+const exportSaveBtn = document.querySelector('#exportSave');
+let background = '';
 
 function updateBgInDom() {
-    pages = document.querySelectorAll(".page")
-    for (page of pages) {
-        page.style.backgroundImage = `url("/noise.e486a8eb.png"), linear-gradient(0deg, rgba(43, 30, 62, 0.6), rgba(43, 30, 62, 0.6)), linear-gradient(180deg, rgba(51, 0, 102, 0) 0%, rgba(51, 0, 102, 0.85) 61.46%, rgba(51, 0, 102, 0.933954) 79.69%, #330066 99.99%, #330066 100%, #330066 100%), url("${background}")`
-    }
+  const pages = document.querySelectorAll('.page');
+
+  pages.forEach((page) => {
+    page.style.backgroundImage = `url('/noise.e486a8eb.png'), linear-gradient(0deg, rgba(43, 30, 62, 0.6), rgba(43, 30, 62, 0.6)), linear-gradient(180deg, rgba(51, 0, 102, 0) 0%, rgba(51, 0, 102, 0.85) 61.46%, rgba(51, 0, 102, 0.933954) 79.69%, #330066 99.99%, #330066 100%, #330066 100%), url('${background}')`;
+  });
+}
+
+function updateBg() {
+  const uploadedImg = backgroundUpload.files[0];
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    background = reader.result;
+    updateBgInDom();
+  };
+  reader.readAsDataURL(uploadedImg);
 }
 
 function updatePagination() {
-    pages = document.querySelectorAll(".page.content")
-    pageNumber = pages.length
-    i = 1
-    for (page of pages) {
-        page.querySelector(".pagination").innerHTML = i + "/" + pageNumber
-        i++
-    }
+  const pages = document.querySelectorAll('.page.content');
+  const pageNumber = pages.length;
+  let i = 1;
+
+  pages.forEach((page) => {
+    page.querySelector('.pagination').innerHTML = `${i}/${pageNumber}`;
+    i += 1;
+  });
 }
 
 function newPageAfter(el) {
-    newPage = document.createElement("div")
-    newPage.classList.add("page")
-    newPage.classList.add("textOnly")
-    newPage.classList.add("content")
+  const newPage = document.createElement('div');
+  newPage.classList.add('page');
+  newPage.classList.add('textOnly');
+  newPage.classList.add('content');
 
-    ilLogo = document.createElement("img")
-    ilLogo.src = "/il-logo.33af7e0c.png"
-    ilLogo.classList.add("ilearned-logo")
+  const ilLogo = document.createElement('img');
+  ilLogo.src = '/il-logo.33af7e0c.png';
+  ilLogo.classList.add('ilearned-logo');
 
-    checkoutLogo = document.createElement("img")
-    checkoutLogo.src = "/checkout-logo.4e9aeb64.png"
-    checkoutLogo.classList.add("checkout-logo")
+  const checkoutLogo = document.createElement('img');
+  checkoutLogo.src = '/checkout-logo.4e9aeb64.png';
+  checkoutLogo.classList.add('checkout-logo');
 
-    editableSpan = document.createElement("span")
-    editableSpan.setAttribute("role", "textbox")
-    editableSpan.setAttribute("contenteditable", "true")
-    editableSpan.classList.add("editableSpan")
-    editableSpan.innerHTML = "You gotta modify this"
+  const editableSpan = document.createElement('span');
+  editableSpan.setAttribute('role', 'textbox');
+  editableSpan.setAttribute('contenteditable', 'true');
+  editableSpan.classList.add('editableSpan');
+  editableSpan.innerHTML = 'You gotta modify this';
 
-    pagination = document.createElement("a")
-    pagination.classList.add("pagination")
+  const pagination = document.createElement('a');
+  pagination.classList.add('pagination');
 
-    locationIndicator = document.createElement("span")
-    locationIndicator.classList.add("location")
-    locationIndicator.setAttribute("contenteditable", "true")
+  const locationIndicator = document.createElement('span');
+  locationIndicator.classList.add('location');
+  locationIndicator.setAttribute('contenteditable', 'true');
 
-    newPage.appendChild(ilLogo)
-    newPage.appendChild(checkoutLogo)
-    newPage.appendChild(editableSpan)
-    newPage.appendChild(pagination)
-    newPage.appendChild(locationIndicator)
+  newPage.appendChild(ilLogo);
+  newPage.appendChild(checkoutLogo);
+  newPage.appendChild(editableSpan);
+  newPage.appendChild(pagination);
+  newPage.appendChild(locationIndicator);
 
-    toolbox = document.createElement("div")
-    toolbox.classList.add("toolbox")
+  const toolbox = document.createElement('div');
+  toolbox.classList.add('toolbox');
 
-    deleteBtn = document.createElement("button")
-    deleteBtn.classList.add("btn")
-    deleteBtn.classList.add("btn-secondary")
-    deleteBtn.setAttribute("onclick", "deletePage(this.parentNode.parentNode)")
-    deleteBtn.innerHTML = "🗑️ Supprimer"
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('btn');
+  deleteBtn.classList.add('btn-secondary');
+  deleteBtn.addEventListener('click', this.remove());
+  deleteBtn.innerHTML = '🗑️ Supprimer';
 
-    newPageBtn = document.createElement("button")
-    newPageBtn.classList.add("btn")
-    newPageBtn.classList.add("btn-secondary")
-    newPageBtn.setAttribute("onclick", "newPageAfter(this.parentNode.parentNode)")
-    newPageBtn.innerHTML = "📄 Nouvelle page après"
+  const newPageBtn = document.createElement('button');
+  newPageBtn.classList.add('btn');
+  newPageBtn.classList.add('btn-secondary');
+  newPageBtn.setAttribute('onclick', 'newPageAfter(this.parentNode.parentNode)');
+  newPageBtn.innerHTML = '📄 Nouvelle page après';
 
-    toolbox.appendChild(deleteBtn)
-    toolbox.appendChild(newPageBtn)
+  toolbox.appendChild(deleteBtn);
+  toolbox.appendChild(newPageBtn);
 
-    pageContainer = document.createElement("div")
-    pageContainer.classList.add("pageContainer")
+  const pageContainer = document.createElement('div');
+  pageContainer.classList.add('pageContainer');
 
-    pageContainer.appendChild(newPage)
-    pageContainer.appendChild(toolbox)
+  pageContainer.appendChild(newPage);
+  pageContainer.appendChild(toolbox);
 
-    el.parentNode.insertBefore(pageContainer, el.nextSibling)
-    updatePagination()
-    updateBgInDom()
+  el.parentNode.insertBefore(pageContainer, el.nextSibling);
+  updatePagination();
+  updateBgInDom();
 }
 
-function deletePage(el) {
-    el.parentNode.removeChild(el)
+function exportAsImages() {
+  const pages = document.querySelectorAll('.page');
+  let i = 0;
+
+  pages.forEach((page) => {
+    htmlToImage.toPng(page)
+      .then((dataUrl) => {
+        download(dataUrl, `${document.querySelector('.page.titlePost span').innerHTML.toLowerCase().replace(/\s/g, '-')}-${i}.png`);
+        i += 1;
+      });
+  });
 }
 
-exportBtn.addEventListener("click", function() {
-    pages = document.querySelectorAll(".page")
-    i = 0
-    for (page of pages) {
-        htmlToImage.toPng(page)
-        .then(function (dataUrl) {
-            download(dataUrl, document.querySelector(".page.titlePost span").innerHTML.toLowerCase().replace(/\s/g , "-") + "-" + i + '.png');
-            i++
-        });
-    }
-})
-
-if (navigator.userAgent.includes("Firefox")) {
-    document.onkeydown = function(ev) {
-        ev = ev || window.ev
-        if (ev.ctrlKey && ev.code == "KeyB") {
-            ev.preventDefault()
-            surround("b")
-        } else if (ev.ctrlKey && ev.code == "KeyI") {
-            ev.preventDefault()
-            surround("i")
-        }
-    }    
-}
+exportBtn.addEventListener('click', exportAsImages);
 
 function surround(elementType) {
-    selection = window.getSelection()
-    if (!selection.focusNode.parentNode.textContent.includes(selection.toString() + selection.focusNode.textContent)) {
-        selection.focusNode.splitText(selection.anchorOffset)
-        selection.focusNode.splitText(selection.focusOffset)
-        selection = window.getSelection()
-        bolded = document.createElement(elementType)
-        bolded.innerHTML = selection.toString()
-        selection.focusNode.parentNode.insertBefore(bolded, selection.focusNode.nextSibling)
-        selection.focusNode.remove()
+  let selection = window.getSelection();
+  const allText = selection.focusNode.parentNode.textContent;
+  const selectedAnNext = selection.toString() + selection.focusNode.textContent;
+  const isSelectingBInFirefox = !allText.includes(selectedAnNext);
+  if (isSelectingBInFirefox) {
+    selection.focusNode.splitText(selection.anchorOffset);
+    selection.focusNode.splitText(selection.focusOffset);
+    selection = window.getSelection();
+    const bolded = document.createElement(elementType);
+    bolded.innerHTML = selection.toString();
+    selection.focusNode.parentNode.insertBefore(bolded, selection.focusNode.nextSibling);
+    selection.focusNode.remove();
+  } else {
+    const el = selection.focusNode.previousSibling;
+    const parent = el.parentNode;
+    while (el.firstChild) parent.insertBefore(el.firstChild, el);
+    parent.removeChild(el);
+    parent.normalize();
+  }
+}
 
-    } else {
-        el = selection.focusNode.previousSibling
-        parent = el.parentNode;
-        while (el.firstChild) parent.insertBefore(el.firstChild, el);
-        parent.removeChild(el);
-        parent.normalize()
+if (navigator.userAgent.includes('Firefox')) {
+  document.onkeydown = (event) => {
+    const ev = event || window.ev;
+    if (ev.ctrlKey && ev.code === 'KeyB') {
+      ev.preventDefault();
+      surround('b');
+    } else if (ev.ctrlKey && ev.code === 'KeyI') {
+      ev.preventDefault();
+      surround('i');
     }
+  };
 }
 
 function exportSave() {
-    pages = document.querySelectorAll(".page:not(.titlePost)")
-    save = {
-        "title": document.querySelector(".page.titlePost span").innerHTML,
-        "pages": [],
-        "background": background
-    }
+  const pages = document.querySelectorAll('.page:not(.titlePost)');
+  const save = {
+    title: document.querySelector('.page.titlePost span').innerHTML,
+    pages: [],
+    background,
+  };
 
-    for (page of pages) {
-        object = {
-            "type": page.classList[1],
-            "content": encodeURI(page.querySelector(".editableSpan").innerHTML),
-            "location": page.querySelector(".location").innerHTML
-        }
-        save.pages.push(object)
-    }
+  pages.forEach((page) => {
+    const object = {
+      type: page.classList[1],
+      content: encodeURI(page.querySelector('.editableSpan').innerHTML),
+      location: page.querySelector('.location').innerHTML,
+    };
+    save.pages.push(object);
+  });
 
-    download(JSON.stringify(save), document.querySelector(".page.titlePost span").innerHTML.toLowerCase().replace(/\s/g , "-") + ".json")
+  download(JSON.stringify(save), `${document.querySelector('.page.titlePost span').innerHTML.toLowerCase().replace(/\s/g, '-')}.json`);
 }
 
 function restoreSave() {
-    uploadedSave = saveUpload.files[0]
-    saveReader = new FileReader()
-    saveReader.onloadend = function() {
-        save = JSON.parse(saveReader.result)
-        background = save.background
-        document.querySelector(".page.titlePost span").innerHTML = save.title
+  const uploadedSave = saveUpload.files[0];
+  const saveReader = new FileReader();
+  saveReader.onloadend = () => {
+    const save = JSON.parse(saveReader.result);
+    background = save.background;
+    document.querySelector('.page.titlePost span').innerHTML = save.title;
 
-        for (pageData of save.pages) {
-            actualPages = document.querySelectorAll(".page")
-            newPageAfter(actualPages[actualPages.length - 1].parentNode)
-            page = document.querySelectorAll(".page")[actualPages.length]
-            page.querySelector(".editableSpan").innerHTML = decodeURI(pageData.content)
-            page.querySelector(".location").innerHTML = pageData.location
-        }
+    save.pages.forEach((pageData) => {
+      const actualPages = document.querySelectorAll('.page');
+      newPageAfter(actualPages[actualPages.length - 1].parentNode);
+      const page = document.querySelectorAll('.page')[actualPages.length];
+      page.querySelector('.editableSpan').innerHTML = decodeURI(pageData.content);
+      page.querySelector('.location').innerHTML = pageData.location;
+    });
 
-        updateBgInDom()
-    }
-    saveReader.readAsText(uploadedSave);
+    updateBgInDom();
+  };
+  saveReader.readAsText(uploadedSave);
 }
 
+backgroundUpload.addEventListener('change', updateBg);
+saveUpload.addEventListener('change', restoreSave);
+exportSaveBtn.addEventListener('click', exportSave);
